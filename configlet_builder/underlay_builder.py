1dba89111f9e1384e38c315d2aea010c780d9b9f
arista <atd-help@arista.com>
2022-03-02 15:49:27 +0000

    Added a new fucntion

--- configlet_builder/underlay_builder.py
+++ configlet_builder/underlay_builder.py
@@ -1,5 +1,17 @@
 from cvplibrary import CVPGlobalVariables, GlobalVariableNames
+import yaml
+from cvplibrary import RestClient
+import ssl
 
+def get_yaml(configlet):
+  ssl._create_default_https_context = ssl._create_unverified_context
+  cvp_url = 'https://192.168.0.5/cvpservice/'
+  client = RestClient(cvp_url+'configlet/getConfigletByName.do?name='+configlet,'GET')
+  if client.connect():
+    raw = yaml.load(client.getResponse())
+    yaml_raw = raw['config']
+    yaml_dict = yaml.safe_load(yaml_raw)
+    return yaml_dict
 
 def get_hostname():
   stuff = CVPGlobalVariables.getValue(GlobalVariableNames.CVP_SYSTEM_LABELS)
@@ -9,6 +21,8 @@ def get_hostname():
       hostname = value
   return hostname
   
-hostname = get_hostname()
+  
+underlay = get_yaml('underlay_yaml')
 
-print(hostname)
+
+
